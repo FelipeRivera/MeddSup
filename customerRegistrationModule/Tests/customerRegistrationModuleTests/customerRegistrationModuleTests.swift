45379dee67ff6eacc482b6f6427960d6de5c391b
FelipeRivera94 <yf.rivera1851@uniandes.edu.co>
2025-11-13 21:10:18 -0500

    test

--- customerRegistrationModule/Tests/customerRegistrationModuleTests/customerRegistrationModuleTests.swift
+++ customerRegistrationModule/Tests/customerRegistrationModuleTests/customerRegistrationModuleTests.swift
@@ -1,6 +1,11 @@
+//
+//  customerRegistrationModuleTests.swift
+//  customerRegistrationModuleTests
+//
+//  Created by Felipe Rivera on 3/09/25.
+//
+
 import Testing
 @testable import customerRegistrationModule
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
+// Main test file - actual tests are in CustomerRegistrationViewModelTests.swift
